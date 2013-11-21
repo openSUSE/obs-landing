5e2df8b3858081d03fe48e6ef422b2b5194d4952
Adrian Schröter <adrian@suse.de>
2013-11-21 13:34:37 +0100

    - use right git submodule for docu

--- update_documentation.sh
+++ update_documentation.sh
@@ -3,27 +3,28 @@
 git submodule init
 git submodule update
 
-pushd open-build-service-documentation/
+for doc in obs-best-practices obs-reference-guide; do
+  pushd open-build-service-documentation/
 
-  for doc in obs-best-practices obs-reference-guide; do
-    source ./ENV-$doc || exit 1
-    rm -rf html epub
-    make html epub || exit 1
+  source ./DC-$doc || exit 1
+  rm -rf build 
+  daps html  || exit 1
 
-    # update html
-    rm -rf ../help/manuals/$doc || exit 1
-    cp -aL html/$doc ../help/manuals/$doc || exit 1
-    rm -r html/$doc
+  # update html
+  rm -rf ../help/manuals/$doc || exit 1
+  cp -aL build/$doc/html//$doc ../help/manuals/$doc || exit 1
+  rm -r build/$doc
 
-    # update epub
-    rm ../files/manuals/${doc}.epub || exit 1
-    mv epub/${doc}.xml ../files/manuals/${doc}.epub || exit 1
+  # update epub
+#   rm ../files/manuals/${doc}.epub || exit 1
+#   mv epub/${doc}.xml ../files/manuals/${doc}.epub || exit 1
+  popd
 
-    # add to git 
-    git add help/manuals/$doc files/manuals/$doc.epub || exit 1
-  done
+  # add to git 
+  #git add help/manuals/$doc files/manuals/$doc.epub || exit 1
+  git add help/manuals/$doc || exit 1
+done
 
-popd
 
 git add open-build-service-documentation || exit 1
 git commit -m "Update books to current state" || exit 1
