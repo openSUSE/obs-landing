87b01c6b9185290a542b212f25c7aa5ebbdfab9b
Adrian Schröter <adrian@suse.de>
2016-02-19 10:14:12 +0100

    Update books to current state

--- help/manuals/obs-reference-guide/static/js/script.js
+++ help/manuals/obs-reference-guide/static/js/script.js
@@ -1,85 +1,39 @@
+/*
+ * JavaScript for SUSE documentation
+ *
+ * Author:
+ *   Adam Spiers
+ *
+ * Contributors:
+ *   Thomas Schraitle
+ *
+*/
+
 var active = false;
 var deactivatePosition = -1;
 
 // Parts of the bug reporter, (c) Adam Spiers
-var XmlProduct = $( 'meta[name="product-name"]' ).attr( 'content' ) + ' ' + $( 'meta[name="product-number"]' ).attr( 'content' )
-var bugzillaProduct = 0;
-var bugzillaComponent = 'Documentation';
+var trackerURL = $( 'meta[name="tracker-url"]' ).attr('content')
+var trackerType = $( 'meta[name="tracker-type"]' ).attr('content')
 
-// XmlProduct comes from DocBook: <productname/> [space] <productnumber/>
-// BugzillaProduct comes from Bugzilla, go to
-// https://bugzilla.suse.com/enter_bug.cgi , search for the product,
-// and write down the exact product name as given there. Click on the
-// product name in Bugzilla, and find out what the component for documentation
-// is called, that is your bugzillaComponent (default is "Documentation")
-
-
-switch ( XmlProduct ) {
-  case 'SUSE Cloud 2.0':
-    bugzillaProduct = 'SUSE Cloud 2.0';
-    break;
-  case 'SUSE Cloud 3':
-    bugzillaProduct = 'SUSE Cloud 3';
-    break;
-  case 'Subscription Management Tool 11.3':
-    bugzillaProduct = 'Subscription Management Tool 11 SP3 (SMT 11 SP3)';
-    bugzillaComponent = 'SMT';
-    break;
-  case 'SUSE Lifecycle Management Server 1.3':
-    bugzillaProduct = 'SUSE Lifecycle Management Server';
-    break;
-  case 'SUSE Linux Enterprise Desktop 12':
-    bugzillaProduct = 'SUSE Linux Enterprise Desktop 12';
-    break;
-  case 'SUSE Linux Enterprise Desktop 11 SP3':
-    bugzillaProduct = 'SUSE Linux Enterprise Desktop 11 SP3';
-    break;
-  case 'SUSE Linux Enterprise Point of Service 11 SP2':
-    bugzillaProduct = 'SUSE Linux Enterprise Point of Service 11 SP2 (SLEPOS 11 SP2)';
-    break;
-  case 'SUSE Linux Enterprise Server 12':
-    bugzillaProduct = 'SUSE Linux Enterprise Server 12 (SLES 12)';
-    break;
-  case 'SUSE Linux Enterprise Server 11 SP3':
-    bugzillaProduct = 'SUSE Linux Enterprise Server 11 SP3 (SLES 11 SP3)';
-    break;
-  case 'SUSE Linux Enterprise Server for SAP Applications 11 SP3':
-    bugzillaProduct = 'SUSE Linux Enterprise for SAP Applications 11 SP3';
-    bugzillaComponent = 'General';
-    break;
-//for NFS Quick Start, productnumber is 11 instead on 11 SP3 (PM's wish)
-  case 'SUSE Linux Enterprise High Availability Extension 11':
-    bugzillaProduct = 'SUSE Linux Enterprise High Availability Extension 11 SP3';
-    break;
-  case 'SUSE Linux Enterprise High Availability Extension 11 SP3':
-    bugzillaProduct = 'SUSE Linux Enterprise High Availability Extension 11 SP3';
-    break;
-  case 'SUSE Linux Enterprise High Availability Extension 12':
-    bugzillaProduct = 'SUSE Linux Enterprise High Availability Extension 12';
-    break;
-  case 'GEO Clustering for SUSE Linux Enterprise High Availability Extension 12':
-    bugzillaProduct = 'SUSE Linux Enterprise High Availability Extension 12';
-    break;
-  case 'SUSE Manager 1.7':
-    bugzillaProduct = 'SUSE Manager 1.7 Server';
-    break;
-  case 'SUSE Manager 2.1':
-    bugzillaProduct = 'SUSE Manager 2.1 Server';
-    break;
-  case 'SUSE Studio Onsite 1.3':
-    bugzillaProduct = 'SUSE Studio Onsite';
-    break;
-  case 'WebYaST 11':
-    bugzillaProduct = 'WebYaST';
-    bugzillaComponent = 'Documenation'; // sic!
-    break;
+// we handle Github (= gh) and bugzilla.suse.com (= bsc), default to bsc
+if ((trackerType != 'gh') && (trackerType != 'bsc')) {
+  trackerType = 'bsc';
 }
 
-var bugzillaURLprefix = 'https://bugzilla.suse.com/enter_bug.cgi?&product=' + encodeURIComponent(bugzillaProduct) + '&component=' + encodeURIComponent(bugzillaComponent);
+// For Bugzilla
+var bscComponent = $( 'meta[name="tracker-bsc-component"]' ).attr('content')
+if (!bscComponent) {
+  bscComponent = 'Documentation'; // default component
+}
+var bscProduct = $( 'meta[name="tracker-bsc-product"]' ).attr('content')
+var bscAssignee = $( 'meta[name="tracker-bsc-assignee"]' ).attr('content')
+// For GitHub
+var ghAssignee = $( 'meta[name="tracker-gh-assignee"]' ).attr('content')
+var ghLabels = $( 'meta[name="tracker-gh-labels"]' ).attr('content')
 
 
 $(function() {
-
   /* http://css-tricks.com/snippets/jquery/smooth-scrolling/ */
   var speed = 400;
 
@@ -180,25 +134,63 @@ $(function() {
     $('#_toolbar').addClass('only-nav');
   }
 
-  if ( bugzillaProduct != 0 ) {
-    $('.permalink:not([href^=#idm])').each(function () {
-        var permalink = this.href;
-        var sectionNumber = "";
-        var sectionName = "";
-        if ( $(this).prevAll('span.number')[0] ) {
-          sectionNumber = $(this).prevAll('span.number')[0].innerHTML;
-        }
-        if ( $(this).prevAll('span.number')[0] ) {
-          var sectionName = $(this).prevAll('span.name')[0].innerHTML;
-        }
-        var body = sectionNumber + " " + sectionName + "\n\n" + permalink;
-        var URL = bugzillaURLprefix + "&short_desc=[doc]+&comment=" + encodeURIComponent(body);
-        $(this).before("<a class=\"report-bug\" target=\"_blank\" href=\"" + URL + "\" title=\"Report a bug against this section\">Report Bug</a> ");
-    });
-  }
-
+  tracker()
 });
 
+
+function tracker() {
+  // do not create links if there is no URL
+  if ( typeof(trackerURL) == 'string') {
+    $('.permalink:not([href^=#idm])').each(function () {
+      var permalink = this.href;
+      var sectionNumber = "";
+      var sectionName = "";
+      var URL = "";
+      if ( $(this).prevAll('span.number')[0] ) {
+        sectionNumber = $(this).prevAll('span.number')[0].innerHTML;
+      }
+      if ( $(this).prevAll('span.number')[0] ) {
+        sectionName = $(this).prevAll('span.name')[0].innerHTML;
+      }
+
+      if (trackerType == 'bsc') {
+        URL = bugzilla(sectionNumber, sectionName, permalink);
+      }
+      else {
+        URL = github(sectionNumber, sectionName, permalink);
+      }
+
+      $(this).before("<a class=\"report-bug\" target=\"_blank\" href=\""
+        + URL
+        + "\" title=\"Report a bug against this section\">Report Bug</a> ");
+    });
+  }
+}
+
+function github(sectionNumber, sectionName, permalink) {
+  var labels = ghLabels.split(",")
+  var body = sectionNumber + " " + sectionName + "\n\n" + permalink;
+  var URL = trackerURL + "?title=" + encodeURIComponent(sectionName)
+     + "&body=" + encodeURIComponent(body);
+  if (ghAssignee) {
+    URL += "&assignee=" + encodeURIComponent(ghAssignee);
+  }
+  for(var i=0; i< labels.length; i++) {
+    URL += "&labels[]=" + labels[i];
+  }
+  return URL;
+}
+
+function bugzilla(sectionNumber, sectionName, permalink) {
+  var body = sectionNumber + " " + sectionName + "\n\n" + permalink;
+  var URL = trackerURL + "?&product=" + encodeURIComponent(bscProduct)
+    + '&component=' + encodeURIComponent(bscComponent)
+    + "&short_desc=[doc]+&comment=" + encodeURIComponent(body);
+  if (bscAssignee) {
+    URL += "&assigned_to=" + encodeURIComponent(bscAssignee);
+  }
+  return URL;
+}
 
 function activate( elm ) {
   var element = elm;
