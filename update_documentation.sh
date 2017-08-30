4afbed66746ef27d81a4bd08e51a571f57178d68
Henne Vogelsang <hvogel@opensuse.org>
2017-08-30 17:24:47 +0200

    Adopt to the new manuals

--- update_documentation.sh
+++ update_documentation.sh
@@ -8,7 +8,7 @@ git fetch
 git checkout origin/master
 popd
 
-for doc in obs-best-practices obs-reference-guide obs-admin-guide; do
+for doc in obs-admin-guide obs-beginners-guide obs-best-practices obs-reference-guide obs-user-guide; do
   pushd open-build-service-documentation/
 
   source ./DC-$doc || exit 1
@@ -18,29 +18,21 @@ for doc in obs-best-practices obs-reference-guide obs-admin-guide; do
   daps pdf || exit 1
 
   # update html
-  rm -rf ../help/manuals/$doc || exit 1
-  cp -aL build/$doc/html/$doc ../help/manuals/$doc || exit 1
+  rm -rf ../help/manuals/${doc} || exit 1
+  cp -aL build/${doc}/html/$ROOTID ../help/manuals/${doc} || exit 1
 
   # update epub
-  rm ../files/manuals/${doc}.epub || exit 1
-  mv build/$doc/${doc}_en.epub ../files/manuals/${doc}.epub || exit 1
+  mv build/${doc}/${ROOTID}_en.epub ../files/manuals/${doc}.epub || exit 1
 
   # update pdf
-  rm ../files/manuals/${doc}.pdf || exit 1
-  mv build/$doc/${doc}_color_en.pdf ../files/manuals/${doc}.pdf || exit 1
+  mv build/${doc}/${ROOTID}_color_en.pdf ../files/manuals/${doc}.pdf || exit 1
 
   #cleanup
-  rm -r build/$doc
+  rm -r build/${doc}
   popd
 
   # add to git 
-  git add help/manuals/$doc files/manuals/$doc.epub || exit 1
-  git add help/manuals/$doc || exit 1
+  git add help/manuals/$doc files/manuals/$doc.epub files/manuals/$doc.pdf || exit 1
 done
 
-
-git add open-build-service-documentation || exit 1
-git add open-build-service-documentation || exit 1
-git add files/manuals/*.pdf || exit 1
 git commit -m "Update books to current state" || exit 1
-
