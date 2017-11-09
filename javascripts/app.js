faf9d0ecd7e29c7c843d2dc26df9e58a5a0033a2
mdeniz <mdeniz@suse.com>
2017-11-09 18:20:40 +0100

    Replace menu and logo with Semantic UI classes and javascript

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,2 +1,13 @@
-(function($){
-})(jQuery);
+$(document)
+  .ready(function() {
+
+    // fix main menu to page on passing
+    $('.main.menu').visibility({
+      type: 'fixed'
+    });
+    
+    // show dropdown on hover
+    $('.main.menu  .ui.dropdown').dropdown({
+      on: 'hover'
+    });
+  });
