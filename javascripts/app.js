010c54a98e785b4ddcd316d5b7101cacc1dbe984
mdeniz <mdeniz@suse.com>
2017-11-10 01:28:23 +0100

    Redesign the home page

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,24 @@
 $(document)
   .ready(function() {
 
-    // fix main menu to page on passing
-    $('.main.menu').visibility({
-      type: 'fixed'
-    });
-    
+    // fix menu when passed
+    $('.masthead')
+      .visibility({
+        once: false,
+        onBottomPassed: function() {
+          $('.fixed.menu').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+          $('.fixed.menu').transition('fade out');
+        }
+      });
+
+    // create sidebar and attach to menu open
+    $('.ui.sidebar')
+      .sidebar('attach events', '.toc.item');
+
     // show dropdown on hover
-    $('.main.menu  .ui.dropdown').dropdown({
+    $('.masthead  .ui.dropdown').dropdown({
       on: 'hover'
     });
   });
