3ad2d36cd99cc100613ee52cca78e13ddb2ba768
Adrian Schröter <adrian@suse.de>
2020-08-14 11:22:07 +0200

    daps on 15.2 is broken in multiple ways atm
    so switching to daps2docker for now

--- update_documentation.sh
+++ update_documentation.sh
@@ -11,24 +11,50 @@ popd
 for doc in obs-admin-guide obs-user-guide; do
   pushd open-build-service-documentation/
 
-  source ./DC-$doc || exit 1
-  rm -rf build 
-  daps html || exit 1
-  daps epub || exit 1
-  daps pdf || exit 1
+  ROOTID=${doc%-guide}
 
-  # update html
-  rm -rf ../help/manuals/${doc} || exit 1
-  cp -aL build/${doc}/html/$ROOTID ../help/manuals/${doc} || exit 1
+# plain daps is broken on 15.2 in multiple places
+  if false; then
+    source ./DC-$doc || exit 1
+    rm -rf build 
+    daps --verbosity=3 html || exit 1
+    daps --verbosity=3 epub || exit 1
+    daps --verbosity=3 pdf || exit 1
+    # update html
+    rm -rf ../help/manuals/${doc} || exit 1
+    cp -aL build/${doc}/html/$ROOTID ../help/manuals/${doc} || exit 1
 
-  # update epub
-  mv build/${doc}/${ROOTID}_en.epub ../files/manuals/${doc}.epub || exit 1
+    # update epub
+    mv build/${doc}/${ROOTID}_en.epub ../files/manuals/${doc}.epub || exit 1
 
-  # update pdf
-  mv build/${doc}/${ROOTID}_color_en.pdf ../files/manuals/${doc}.pdf || exit 1
+    # update pdf
+    mv build/${doc}/${ROOTID}_color_en.pdf ../files/manuals/${doc}.pdf || exit 1
 
-  #cleanup
-  rm -r build/${doc}
+    #cleanup
+    rm -r build/${doc}
+  else
+    # using docker container instead
+
+    # urgs cleanup
+    rm -rf /tmp/daps2docker-*
+
+    /usr/bin/daps2docker DC-$doc || exit 1  # html and pdf
+    /usr/bin/daps2docker DC-$doc epub || exit 1
+
+    # update html
+    rm -rf ../help/manuals/${doc} || exit 1
+    cp -aL /tmp/daps2docker-*/$doc/html/book.$ROOTID ../help/manuals/${doc} || exit 1
+
+    # update epub
+    mv /tmp/daps2docker-*/$doc/book.${ROOTID}_en.epub ../files/manuals/${doc}.epub || exit 1
+
+    # update pdf
+    mv /tmp/daps2docker-*/$doc/book.${ROOTID}_color_en.pdf ../files/manuals/${doc}.pdf || exit 1
+
+    #cleanup
+    rm -rf /tmp/daps2docker-*
+  fi
+
   popd
 
   # add to git 
